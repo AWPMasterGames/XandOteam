ed18bb475802f72a774a9911ab104ef0bb206d37
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2023-05-11 17:17:01 +0300

    visula
    в файлах visual код для визуалки где были добавлены функций показа поля и начального текста.
    .printfield(адрес массива) показывает поле.
    .starttext() стартовый текс, его нужно ставить в самом начале для выбора режима.
    
    в файлах visualtest там тестовые функций для проверки работы правильно показывается поле.
    там есть 3 функций: 
    void test1(адрес массива) заполнения с лево на право
    void test2(адрес массива) заполнение с лево сверху по право снизу
    void test3(адрес массива) заполнение с верху в низ

---
+++ Visual.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iostream>
+#include <time.h>
+#include <string>
+#include "visual.h"
+#include "visualtest.h"
+
+using namespace std;
+int a[3][3];
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	Visual visual;
+	Test test;
+	visual.starttext();
+	visual.printfield(a);
+	test.test3(a);
+	visual.printfield(a);
+	
+}
+
+void Visual::starttext()
+{
+	cout << "Какой режим игры хотите выбрать?\n";
+	cout << "1 - Игрок против Игрока\n2 - Игрок против ПК\n2 - Пк против Пк\n\n";
+}
+
+void Visual::printfield(int a[][3])
+{
+	for (int j = 0; j < 3; j++) {
+		cout << "| ";
+		for (int k = 0; k < 3; k++) {
+			if (a[j][k] == 2) {
+				cout << "X" << " | ";
+			}
+			else if (a[j][k] == 3)
+				cout << "O" << " | ";
+			else
+				cout << " " << " | ";
+		}
+		cout << endl;
+	}
+	cout << "\n\n\n\n\n\n\n\n";
+}
