9aed9dc715bc03dbdfb548b9d2f3e3ed281b78ba
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-12 18:22:07 +0300

    skeleton

---
+++ Game.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+#include "Game.h"
+#include "Modes.h"
+using namespace std;
+
+int arr[3][3];
+
+class I_game {
+public:
+	virtual void start();
+	virtual void set();
+	void select() {
+		int a;
+		cout << "Выберите вид игры:\n1 - Игрок простив игрока\n2 - Игрок против компьютера\n3 - Компьютер против компьютера";
+		cin >> a;
+		switch (a){
+		case 1: {
+			PvP player1;
+			player1.start();
+			break;
+		}
+		case 2:{
+			PvC player2;
+			player2.start();
+			break;
+		}
+		case 3: {
+			CvC player3;
+			player3.start();
+			break; 
+		}
+		default:
+			break;
+		}
+	}
+};
---
+++ Modes.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+using namespace std;
+
+//int arr[3][3];
+
+class PvP : public IGame {
+public:
+	int scale = 0;
+	void start() {
+		cout << "Вы выбрали режим игрок против игрока.\n";
+	}
+	void set(int arr[][3]) {
+		int a, b;
+		cout << "Куда хотите поставить свой символ?\n";
+		cout << "По ряду: ";
+		cin >> a;
+		cout << "По столбцу: ";
+		cin >> b;
+		if (scale % 2 == 1) {
+			arr[a - 1][b - 1] = 3;
+		}
+		else
+			arr[a - 1][b - 1] = 2;
+		scale++;
+	}
+};
+
+class PvC : public IGame {
+public:
+	void start() {
+		cout << "Вы выбрали режим игрок против компьютера.\n";
+	}
+	void set(int arr[][3]) {
+		int a, b;
+		cout << "Куда хотите поставить свой символ?\n";
+		cout << "По ряду: ";
+		cin >> a;
+		cout << "По столбцу: ";
+		cin >> b;
+		arr[a - 1][b - 1] = 3;
+	}
+	void set_pc(int arr[][3]) {
+		srand(time(NULL));
+		arr[rand() % 2][rand() % 2] = 2;
+	}
+};
+
+class CvC : public IGame {
+public:
+	void start() {
+		cout << "Вы выбрали режим компьютер против компьютера.\n";
+	}
+	void set_pc(int arr[][3]) {
+		srand(time(NULL));
+		arr[rand() % 2][rand() % 2] = 2;
+	}
+};
