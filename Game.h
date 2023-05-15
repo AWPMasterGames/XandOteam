8dadcb4e87d60ca53f01fb2365bca02c211b991d
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-15 16:04:39 +0300

    Game

--- Game.h
+++ Game.h
@@ -6,6 +6,6 @@ using namespace std;
 class IGame {
 public:
 	virtual void start() {};               
-	virtual void set() {};                 
+	virtual void set() {};              
 	//virtual void pause() {};                 
 };
