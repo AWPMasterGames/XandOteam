3005749c7a668db723ab0565af5c9e2f08fd9cd6
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2023-05-15 15:59:32 +0300

    Delete visualtest.h

--- visualtest.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Test {
-public:
-	void test1(int a[][3]); // заполнения с лево на право
-	void test2(int a[][3]); // заполнение с лево сверху по право снизу
-	void test3(int a[][3]); // заполнение с верху в низ
-
-
-};
