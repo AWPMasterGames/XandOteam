9d4ed3570b17845c6e968d74e149d56056569f2a
Xantrall <119814263+Xantrall@users.noreply.github.com>
2023-05-15 18:10:46 +0300

    Save

---
+++ Save/Save.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "Save.h"
+void Save::saveFile(int a[][3], int hod)
+{
+	doh = hod;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			b[i][j] = a[i][j];
+		}
+	}
+}
+
+void Save::outFile(int a[][3], int hod)
+{
+	hod = doh;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			a[i][j] = b[i][j];
+		}
+	}
+}
---
+++ Save/Save.h
@@ -0,0 +1,10 @@
+#pragma once
+class Save
+{
+	int b[3][3];
+	int doh;
+public:
+	void saveFile(int a[][3], int hod); //аепер дюммше хг хцпш х янупюмъер деиярбхъ
+	void outFile(int a[][3], int hod); //бнгбпюыюер дюммше йнрнпше нм янупюмхк
+};
+
