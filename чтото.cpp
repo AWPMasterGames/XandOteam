76a6bfe4535353e794ca437093647a7f3e6d245a
Mggjd <128396846+Mggjd@users.noreply.github.com>
2023-05-15 16:47:04 +0300

    Add files via upload
    проверка на ничью на победу и пройгрышь

---
+++ чтото.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class GraphicEngineStub {
+private:
+	void CheckDraw(); // проверка на ничью
+	void CheckWin(); // проверка на выйгрышь 
+	void CheckLoss(); // проверка на проигрышь
+	void ChackSymbol(); // проверка на то, есть ли символ в клетке или нет
+	void printtext(); // после проверки пишет ответ
+};
+
+int main() {
+
+}
