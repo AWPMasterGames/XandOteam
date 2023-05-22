ea2b5908193c2a9b77d8f3867fa321203932464c
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-22 09:52:54 +0300

    PE

--- game/AIPlayer.cpp
+++ game/AIPlayer.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <time.h>
 
-void AIPlayer::_turn(int arr[][3], int symbolnumber) {
+void AIPlayer::Turn(int field[][3], int symbolnumber) {
 	srand(time(NULL));
 	while (true) {
 		int row, column;
 		row = rand() % 3;
 		column = rand() % 3;
-		if (arr[row][column] == 0) {
-			arr[row][column] = symbolnumber;
+		if (field[row][column] == 0) {
+			field[row][column] = symbolnumber;
 			break;
 		}
 	}
