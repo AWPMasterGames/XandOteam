c88d0c52e39b69f29a4326d8c94f760c7d74443e
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-22 09:54:54 +0300

    PE

--- game/main.cpp
+++ game/main.cpp
@@ -3,12 +3,12 @@
 #include "IGame.h"
 using namespace std;
 
-int arr[3][3];
+int field[3][3];
 
 int main() {
 	srand(time(NULL));
 	setlocale(LC_ALL, "");
 
 	IGame game;
-	game._select(arr);
+	game.Select(field);
 }
