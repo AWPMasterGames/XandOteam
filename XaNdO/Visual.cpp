f9c2f349b234db38b8c8e7d41ce285c0e732da5a
AWPMasterGames <126659812+AWPMasterGames@users.noreply.github.com>
2023-05-22 16:59:27 +0300

    Update Visual.cpp

--- XaNdO/Visual.cpp
+++ XaNdO/Visual.cpp
@@ -1,30 +1,13 @@
-﻿#include <iostream>
 #include <iostream>
 #include <time.h>
 #include <string>
-#include "visual.h"
 
 using namespace std;
-void Visual::starttext()
-{
-	cout << "Какой режим игры хотите выбрать?\n";
-	cout << "1 - Игрок против Игрока\n2 - Игрок против ПК\n2 - Пк против Пк\n\n";
-}
 
-void Visual::printfield(int a[][3])
-{
-	for (int j = 0; j < 3; j++) {
-		cout << "| ";
-		for (int k = 0; k < 3; k++) {
-			if (a[j][k] == 2) {
-				cout << "X" << " | ";
-			}
-			else if (a[j][k] == 3)
-				cout << "O" << " | ";
-			else
-				cout << " " << " | ";
-		}
-		cout << endl;
-	}
-	cout << "\n\n\n\n\n\n\n\n";
-}
+class Visual {
+public:
+    void StartText(); // стартовый текст
+    int InpuEngine(); // инпут выбора режима
+    void PrintField(int a[][3]);  // показать поле
+
+};
