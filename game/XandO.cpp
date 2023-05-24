c211f523df26b8121d316564f7505473ddec28a5
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-24 09:35:39 +0300

    PE

--- game/XandO.cpp
+++ game/XandO.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <time.h>
 #include "IGame.h"
+
 using namespace std;
 
 int field[3][3];
 
 int main() {
-	srand(time(NULL));
 	setlocale(LC_ALL, "");
+	IGame igame;
+	igame.Select(field);
 
-	IGame game;
-	game.Select(field);
 }
