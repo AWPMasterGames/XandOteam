bc7c470f0116b093f4f513d7fed5de42525d7f02
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-24 09:33:46 +0300

    PE

--- game/HumanPlayer.cpp
+++ game/HumanPlayer.cpp
@@ -1,18 +1,34 @@
-#include "HumanPlayer.h"
+#include "Humanplayer.h"
 #include <iostream>
 using namespace std;
 
-void HumanPlayer::Turn(int field[][3], int symbolnumber) {
+void HumanPlayer::Turn(int arr[][3], int symbolnumber) {
 	while (true) {
-		int row, column;
-		cout << "Êóäà õîòèòå ïîñòàâèòü ñèìâîë?\n";
-		cout << "Ïî ðÿäó: ";
-		cin >> row;
-		cout << "Ïî ñòîëáöó: ";
-		cin >> column;
-		if (field[row - 1][column - 1] == 0) {
-			field[row - 1][column - 1] = symbolnumber;
+		cout << "Хотите поставить игру на паузу?" << endl << "1 - Да\n2 - Нет" << endl;
+		int choise;
+		cin >> choise;
+		switch (choise) {
+		case 1: {
+
 			break;
 		}
+		case 2: {
+
+			break;
+		}
+		default:
+			break;
+		}
+		int row, column;
+		cout << "Куда хотите поставить символ?\n";
+		cout << "По ряду: ";
+		cin >> row;
+		cout << "По столбцу: ";
+		cin >> column;
+		if (arr[row - 1][column - 1] == 0 && row < 4 && row>0 && column < 4 && column >0) {
+			arr[row - 1][column - 1] = symbolnumber;
+			break;
+		}
+		cout << "Неверно выбрано поле" << endl;
 	}
 }
