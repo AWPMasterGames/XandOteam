1c206937048d95d2aba76d9cb4436994d0faf833
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-24 13:42:36 +0300

    PE

--- game/IGame.h
+++ game/IGame.h
@@ -3,6 +3,7 @@
 class IGame {
 public:
 	void Select(int field[][3]);
+private:
 	void GamePlayerVsPlayer(int field[][3]);
 	void GamePlayerVsComputer(int field[][3]);
 	void GameComputerVsPlayer(int field[][3]);
