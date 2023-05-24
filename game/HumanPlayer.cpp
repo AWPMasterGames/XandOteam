566d9f0c049a19f094bad8b65ff851b7f9a4691b
seregaSkrrr <128396175+seregaSkrrr@users.noreply.github.com>
2023-05-24 13:40:56 +0300

    PE

--- game/HumanPlayer.cpp
+++ game/HumanPlayer.cpp
@@ -1,19 +1,30 @@
 #include "Humanplayer.h"
+#include "Save.h"
+#include "IGame.h"
 #include <iostream>
 using namespace std;
 
-void HumanPlayer::Turn(int arr[][3], int symbolnumber) {
+void HumanPlayer::Turn(int field[][3], int symbolnumber) {
 	while (true) {
 		cout << "Хотите поставить игру на паузу?" << endl << "1 - Да\n2 - Нет" << endl;
 		int choise;
 		cin >> choise;
 		switch (choise) {
 		case 1: {
-
-			break;
-		}
-		case 2: {
-
+			Save lox;
+			lox.SaveGame(field, symbolnumber);
+			cout << "Что дальше?\n1 - Продолжить\n2 - Начать заново" << endl;
+			cin >> choise;
+			switch (choise) {
+			case 1: {
+				lox.OutGame(field, symbolnumber);
+				break;
+			}
+			default:
+				IGame Game;
+				Game.Select(field);
+				break;
+			}
 			break;
 		}
 		default:
@@ -25,8 +36,8 @@ void HumanPlayer::Turn(int arr[][3], int symbolnumber) {
 		cin >> row;
 		cout << "По столбцу: ";
 		cin >> column;
-		if (arr[row - 1][column - 1] == 0 && row < 4 && row>0 && column < 4 && column >0) {
-			arr[row - 1][column - 1] = symbolnumber;
+		if (field[row - 1][column - 1] == 0 && row < 4 && row>0 && column < 4 && column >0) {
+			field[row - 1][column - 1] = symbolnumber;
 			break;
 		}
 		cout << "Неверно выбрано поле" << endl;
